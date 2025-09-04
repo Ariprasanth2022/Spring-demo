1e149f3e7e8f6ac48d3a6fea52e5f5cdf6c0795c
ariprasanth <ariprasanthrr03@gmail.com>
2025-09-04 17:08:16 +0530

    adding data

--- src/main/java/com/jspiders/spring/App.java
+++ src/main/java/com/jspiders/spring/App.java
@@ -3,12 +3,14 @@ package com.jspiders.spring;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.jspiders.spring.dto.Product;
 import com.jspiders.spring.dto.Student;
 
 public class App {
     public static void main(String[] args) {
     	ApplicationContext context = new ClassPathXmlApplicationContext("META-INF/beans.xml");
     	
-    	System.out.println(context.getBean(Student.class));
+    	System.out.println(context.getBean("student3",Student.class));
+    	System.out.println(context.getBean("product3",Product.class));
     }
 }
---
+++ src/main/java/com/jspiders/spring/dto/Product.java
@@ -0,0 +1,10 @@
+package com.jspiders.spring.dto;
+
+import lombok.Data;
+
+@Data
+public class Product {
+	private int productId;
+	private String productName;
+	private String price;
+}
